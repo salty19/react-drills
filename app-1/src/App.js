b20d10440010875276503bee750abc09ea632ea3
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-19 13:37:57 -0700

    input-crashing

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super()
+
+    this.state = {
+      userInput: ''
+    }
+  }
+
+  handlechange(val) {
+    this.setState({ userInput: val})
+  }
+
+  render(){
+    return (
+      <div>
+        <input className="userInput" onChange={ (e) => this.handleChange (e.target.value) } />
+        <span className="results"></span>
+      </div>
+    )
+  }
 }
 
 export default App;
