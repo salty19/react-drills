03dcf3ef175f94188268c94477416f0522a7197e
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-23 19:42:08 -0700

    review-app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,40 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from 'react' 
+import './App.css' 
 
 class App extends Component {
+  constructor() {
+    super()
 
-constructor(){
-  super()
+    this.state = {
+      filterString: '',
+      foods: ['spaghetti', 'ice cream', 'sushi', 'bologna']
+    }
+  }
 
-  this.state = {
-    filterInput: '',
-    foodList: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+  handleChange(filter) {
+    this.setState({ filterString: filter })
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods.filter((element, index) => {
+      return element.includes(this.state.filterString)
+    })
+    .map((element, index) => {
+      return <h2 key={index}> {element} </h2>
+    })
+
+    return (
+      <div className='App'>
+        <input onChange={e => this.handleChange(e.target.value)}
+        type='text' />
+        {foodsToDisplay} 
+      </div>
+    )
   }
 }
 
-handleChange(filter) {
-  this.setState({filterInput: filter})
-}
-
-
-render(){
-  let foodsToDisplay = this.state.foodList
-    .filter((element, index) => {
-      return element.includes(this.state.filterInput)
-    })
-  
-    .map( (element, index) => {
-      return <h2 key={index}>{element}</h2>
-    })
-
-  return (
-    <div>
-      <input onChange={ (e) => this.handleChange (e.target.value)} type="text" />
-      {foodsToDisplay}
-    </div>
-  )
-}
-
-}
-
-export default App;
+export default App 
